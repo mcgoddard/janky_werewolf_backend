15afbcb011732c9f2ea78d388b9563966761d08d
Mike Goddard <mike.goddard@infinityworks.com>
2020-04-15 00:23:22 +0100

    Hide roles from the seer, teams are revealed

--- broadcast_lambda/src/main.rs
+++ broadcast_lambda/src/main.rs
@@ -104,10 +104,14 @@ fn filter_state(player: &types::Player, game_state: types::GameState) -> types::
         if game_state.phase.name != types::PhaseName::End {
             if let Some(mut new_attributes) = new_attributes_option {
                 new_attributes.visible_to = vec![];
-                if p.name != player.name && p.attributes.as_ref().unwrap().alive && p.attributes.as_ref().unwrap().role != types::PlayerRole::Mod && 
-                    !p.attributes.as_ref().unwrap().visible_to.contains(&format!("{:?}", player.attributes.as_ref().unwrap().role.clone())) {
-                    new_attributes.role = types::PlayerRole::Unknown;
-                    new_attributes.team = types::PlayerTeam::Unknown;
+                if p.name != player.name && p.attributes.as_ref().unwrap().alive && p.attributes.as_ref().unwrap().role != types::PlayerRole::Mod {
+                    if !p.attributes.as_ref().unwrap().visible_to.contains(&format!("{:?}", player.attributes.as_ref().unwrap().role.clone())) {
+                        new_attributes.role = types::PlayerRole::Unknown;
+                        new_attributes.team = types::PlayerTeam::Unknown;
+                    }
+                    else if player.attributes.as_ref().unwrap().role == types::PlayerRole::Seer {
+                        new_attributes.role = types::PlayerRole::Unknown;
+                    }
                 }
                 new_player.attributes = Some(new_attributes);
             }
