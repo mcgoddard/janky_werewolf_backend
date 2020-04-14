d31acd817be3302c6426ac5510179aa48335ebc0
Mike Goddard <mikeygiom@gmail.com>
2020-04-14 01:29:40 +0100

    Player role and team filtering

--- broadcast_lambda/src/main.rs
+++ broadcast_lambda/src/main.rs
@@ -58,7 +58,8 @@ fn process_record(record: &types::DDBRecord) {
                                     let new_image: types::GameState = serde_json::from_str(&new_image.data["S"]).unwrap();
                                     let players = new_image.players.clone();
                                     for player in &players {
-                                        broadcast(player, new_image.clone());
+                                        let filtered_state = filter_state(player, new_image.clone());
+                                        broadcast(player, filtered_state);
                                     }
                                 },
                                 None => log::error!("No new image"),
@@ -87,6 +88,29 @@ fn broadcast(player: &types::Player, game_state: types::GameState) {
         Err(e) => log::error!("{:?}", e),
         _ => (),
     }
+}
+
+fn filter_state(player: &types::Player, game_state: types::GameState) -> types::GameState {
+    let mut new_state = game_state.clone();
+    if game_state.phase.name == types::PhaseName::Werewolf && 
+        !vec![types::PlayerRole::Mod, types::PlayerRole::Werewolf].contains(&player.attributes.as_ref().unwrap().role) {
+            new_state.phase.data = HashMap::new();
+    }
+    new_state.players = new_state.players.into_iter().map(|p| {
+        let new_attributes_option = p.attributes.clone();
+        let mut new_player = p.clone();
+        if let Some(mut new_attributes) = new_attributes_option {
+            new_attributes.visible_to = vec![];
+            if p.name != player.name && p.attributes.as_ref().unwrap().role != types::PlayerRole::Mod && 
+                !p.attributes.as_ref().unwrap().visible_to.contains(&format!("{:?}", player.attributes.as_ref().unwrap().role.clone())) {
+                new_attributes.role = types::PlayerRole::Unknown;
+                new_attributes.team = types::PlayerTeam::Unknown;
+            }
+            new_player.attributes = Some(new_attributes);
+        }
+        return new_player;
+    }).collect();
+    return new_state;
 }
 
 fn endpoint() -> String {
