4de5dd0c1251c7a0ca332b28f99918d855b5080a
Mike Goddard <mike.goddard@infinityworks.com>
2020-12-02 15:39:05 +0000

    Remove terraform resource cycle

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -42,7 +42,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
 }
 
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
-  name              = "/aws/lambda/${aws_lambda_function.lambda.name}"
+  name              = "/aws/lambda/${var.environment}-${var.lambda_name}"
   retention_in_days = 14
 
   tags = {
