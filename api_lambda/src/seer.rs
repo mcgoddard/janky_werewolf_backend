f83fc6168bc7dd912abc5eb03781188faef501fa
Mike Goddard <mikeygiom@gmail.com>
2020-12-22 22:57:22 +0000

    Added the option for the seer to skip by not submitting a player name

--- api_lambda/src/seer.rs
+++ api_lambda/src/seer.rs
@@ -5,7 +5,7 @@ use crate::ActionError;
 use crate::helpers::{get_state, update_state, living_players_with_role};
 
 #[derive(Deserialize, Serialize, Clone)]
-struct SleepEvent {
+struct SeerEvent {
     action: String,
     data: EventData,
 }
@@ -13,13 +13,13 @@ struct SleepEvent {
 #[derive(Deserialize, Serialize, Clone)]
 struct EventData {
     code: String,
-    player: String,
+    player: Option<String>,
 }
 
 pub fn handle_seer(e: common::ApiGatewayWebsocketProxyRequest) -> Result<(), ActionError> {
     let body = e.body.clone().unwrap();
     info!("{:?}", body);
-    let event: SleepEvent = serde_json::from_str(&body).unwrap();
+    let event: SeerEvent = serde_json::from_str(&body).unwrap();
     
     let table_name = env::var("tableName").unwrap();
 
@@ -31,7 +31,7 @@ pub fn handle_seer(e: common::ApiGatewayWebsocketProxyRequest) -> Result<(), Act
     }
 }
 
-fn move_to_werewolf(event: common::ApiGatewayWebsocketProxyRequest, mut game_state: common::GameState, see_player_name: String)
+fn move_to_werewolf(event: common::ApiGatewayWebsocketProxyRequest, mut game_state: common::GameState, see_player_name: Option<String>)
         -> Result<(), ActionError> {
     let table_name = env::var("tableName").unwrap();
 
@@ -45,24 +45,8 @@ fn move_to_werewolf(event: common::ApiGatewayWebsocketProxyRequest, mut game_sta
     else if players[0].attributes.role != common::PlayerRole::Seer {
         return Err(ActionError::new(&"You are not the seer!".to_string()));
     }
-    let see_player: Vec<common::Player> = game_state.players.clone().into_iter()
-        .filter(|p| p.name == see_player_name).collect();
-    if see_player.len() != 1 {
-        return Err(ActionError::new(&"Invalid player to see!".to_string()));
-    }
-    else if see_player[0].attributes.visible_to.contains(&format!("{:?}", common::PlayerRole::Seer)) || !see_player[0].attributes.alive {
-        return Err(ActionError::new(&"Player is already seen!".to_string()));
-    }
-    let mut new_players = game_state.players.clone();
-    new_players.retain(|p| p.name != see_player_name);
-    let mut new_attributes = see_player[0].attributes.clone();
-    new_attributes.visible_to.push(format!("{:?}", common::PlayerRole::Seer));
-    let mut new_seen_player = see_player[0].clone();
-    new_seen_player.attributes = see_player[0].attributes.clone();
-    new_seen_player.attributes = new_attributes;
-    new_players.push(new_seen_player);
-    game_state.players = new_players.clone();
-    if living_players_with_role(common::PlayerRole::Bodyguard, new_players) > 0 {
+    game_state.players = get_new_players(see_player_name, game_state.clone())?;
+    if living_players_with_role(common::PlayerRole::Bodyguard, game_state.clone().players) > 0 {
         game_state.phase = common::Phase {
             name: common::PhaseName::Bodyguard,
             data: HashMap::new(),
@@ -75,4 +59,27 @@ fn move_to_werewolf(event: common::ApiGatewayWebsocketProxyRequest, mut game_sta
         };
     }
     update_state(game_state, table_name)
+}
+
+fn get_new_players(see_player_name: Option<String>, game_state: common::GameState) -> Result<Vec<common::Player>, ActionError> {
+    if let Some(see_player_name) = see_player_name {
+        let see_player: Vec<common::Player> = game_state.players.clone().into_iter()
+            .filter(|p| p.name == see_player_name).collect();
+        if see_player.len() != 1 {
+            return Err(ActionError::new(&"Invalid player to see!".to_string()));
+        }
+        else if see_player[0].attributes.visible_to.contains(&format!("{:?}", common::PlayerRole::Seer)) || !see_player[0].attributes.alive {
+            return Err(ActionError::new(&"Player is already seen!".to_string()));
+        }
+        let mut new_players = game_state.players;
+        new_players.retain(|p| p.name != see_player_name);
+        let mut new_attributes = see_player[0].attributes.clone();
+        new_attributes.visible_to.push(format!("{:?}", common::PlayerRole::Seer));
+        let mut new_seen_player = see_player[0].clone();
+        new_seen_player.attributes = see_player[0].attributes.clone();
+        new_seen_player.attributes = new_attributes;
+        new_players.push(new_seen_player);
+        return Ok(new_players);
+    }
+    Ok(game_state.players)
 }
